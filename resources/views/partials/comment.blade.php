14a6023fd9c8c144a96de4b81f61d2bf53f8a2d8
Tiago Monteiro <tiagomiguel3221@gmail.com>
2024-11-21 22:22:18 +0000

    post and update comments

---
+++ resources/views/partials/comment.blade.php
@@ -0,0 +1,27 @@
+<li class="comment">
+    <div class="comment-content">
+        <strong>{{ $comment->user->name ?? 'Anonymous' }}</strong>
+        <p>{{ $comment->content }}</p>
+        <small>Posted on: {{ $comment->creation_date }}</small>
+    </div>
+
+    <!-- Reply Form -->
+    <div class="reply-form">
+        <form action="{{ route('comments.store') }}" method="POST">
+            @csrf
+            <textarea name="content" placeholder="Reply to this comment..." required></textarea>
+            <input type="hidden" name="post_id" value="{{ $comment->post_id }}">
+            <input type="hidden" name="parent_comment_id" value="{{ $comment->id }}">
+            <button type="submit">Reply</button>
+        </form>
+    </div>
+
+    <!-- Display Children Comments Recursively -->
+    @if ($comment->children->isNotEmpty())
+        <ul class="children">
+            @foreach ($comment->children as $child)
+                @include('partials.comment', ['comment' => $child])
+            @endforeach
+        </ul>
+    @endif
+</li>
